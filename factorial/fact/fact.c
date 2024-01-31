fb0959273e8dde1ccf0cf597dc89983be78617ca
Jacobo Guillen <jacoboguillen79@gmail.com>
2024-01-30 20:41:52 -0500

    se añade el titulo de evaluación

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -7,9 +7,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
+    <h1>Evaluación</h1>
+
     int number = atoi(argv[1]); //Convert the console input argument to an integer
     
     /*It must be verified that the entered number is greater than or equal to zero*/
+    if(number < 0)
+    {
+        perror("El núnmero debe ser mayor a cero");
+        exit(1);
+    }/*It must be verified that the entered number is greater than or equal to zero*/
     
     /*Implement the function that calculates the factorial of the input argument*/
     
